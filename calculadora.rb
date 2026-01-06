7c2e2435a992964a45cf50141e6a386f378940bd
Osiris Mariano <oslimacontato@gmail.com>
2026-01-06 18:13:46 -0300

    fix: chama método de limpar tela no início do loop

--- calculadora.rb
+++ calculadora.rb
@@ -1,30 +1,44 @@
+def limpar_tela
+  # No Windows o comando é 'cls', no Linux/Mac é 'clear'
+  Gem.win_platform? ? system('cls') : system('clear')
+end
 class Calculadora
   def self.aplicar_desconto(preco, porcentagem)
     return 0 unless preco.is_a?(Numeric) && porcentagem.is_a?(Numeric)
     valor_de_desconto = preco * (porcentagem / 100.0)
+    preco - valor_de_desconto
+  end
+end
+def pedir_numero(mensagem)
+  loop do
+    print mensagem
+    entrada = gets.chomp
+    return entrada.to_f if entrada.match?(/^\d+(\.\d+)?$/)
+    puts "Erro: '#{entrada}' não é um número válido."
+  end
+end
+loop do
+  limpar_tela
+  puts "\n --- Supermarket Pro ---"
+  preco     = pedir_numero("Digite o preço do produto: ")
+  desconto  = pedir_numero("Digite a porcentagem do desconto (%): ")
+  resultado = Calculadora.aplicar_desconto(preco, desconto)
+
+  puts "-------------------------------------"
+  puts "Sucesso! Valor final: #{format('%.2f', resultado)}"
+  puts "-------------------------------------"
+
+  print "\n desenja realizar outro cálculo? (S/N): "
+  decisao = gets.chomp.downcase
+
+  if decisao == 'n' || decisao == 'não' || decisao == 'nao'
+    puts "\nEncerrando o sistema... Áte logo!"
+    break
+    elsif decisao == 's' || decisao == 'sim'
+      puts "Reiniciando..."
+    else
+      puts "Opção inválida, mas vou assuir que você quer sair por segurança."
+      break
   end
 end
 
-loop do
-  puts "\n--- Supermarket Pro: (Módulo de Desconto) ---"
-  puts "Dica: Digite 'sair' no preço para encerrar."
-  
-  print "Digite o preço do produto: "
-  entrada_preco = gets.chomp.downcase
-  
-  # CONDIÇÃO DE SAÍDA:
-  break if entrada_preco == 'sair'
-  
-  preco = entrada_preco.to_f
-  
-  print "Digite a percentagem de desconto (%): "
-  desconto = gets.chomp.to_f
-
-  resultado = Calculadora.aplicar_desconto(preco, desconto)
-
-  puts "-------------------------------"
-  puts "✅ Sucesso! Valor final: R$ #{format('%.2f', resultado)}"
-  puts "-------------------------------"
-end
-
-puts "Obrigado por usar o Supermarket Pro. Até logo!"
